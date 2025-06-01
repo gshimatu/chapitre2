83536126edfb36abba8ceb5f7ed772971a1158ce
gshimatu <gauthiershimatu81@gmail.com>
2025-06-01 22:19:55 +0200

    Création des attributs et du constructeur

--- Chapitre2/PresidentEtudiant.cs
+++ Chapitre2/PresidentEtudiant.cs
@@ -2,8 +2,17 @@ using System;
 
 namespace Chapitre2
 {
-    internal class PresidentEtudiant
+    internal class PresidentEtudiant : Etudiant
     {
-        
+        string mandat;
+        double avantage;
+
+        // Constructeur par défaut
+        public PresidentEtudiant(string nom, string promotion, string faculte, string mandat, double avantage)
+            : base(nom, promotion, faculte)
+        {
+            this.mandat = mandat;
+            this.avantage = avantage;
+        }
     }
 }
