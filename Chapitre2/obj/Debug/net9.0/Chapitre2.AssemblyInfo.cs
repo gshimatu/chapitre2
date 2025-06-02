35905d841bcff05d6c56936ab1f7b9b8e7c15f24
gshimatu <gauthiershimatu81@gmail.com>
2025-06-02 15:32:20 +0200

    Création de la classe PresidentEtudiant2

--- Chapitre2/obj/Debug/net9.0/Chapitre2.AssemblyInfo.cs
+++ Chapitre2/obj/Debug/net9.0/Chapitre2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Chapitre2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a39f57618c7113402d51893996a3e6b43dcdeb67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afd53ae8754b5db7dc89491ebd4e333c78275652")]
 [assembly: System.Reflection.AssemblyProductAttribute("Chapitre2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Chapitre2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
