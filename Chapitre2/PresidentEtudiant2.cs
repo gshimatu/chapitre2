87d7cd4400178415ff4bfa8e13db1ab7909af1bc
gshimatu <gauthiershimatu81@gmail.com>
2025-06-02 15:40:16 +0200

    Implementation de la classe PresidentEtudiant2

--- Chapitre2/PresidentEtudiant2.cs
+++ Chapitre2/PresidentEtudiant2.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,17 @@ namespace Chapitre2
 {
     public class PresidentEtudiant2 : IEtudiant
     {
+        public string nom { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string promotion { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string faculte { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
+        public void AfficherInfos()
+        {
+            throw new NotImplementedException();
+        }
+        public void PayerFrais(double montant)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
